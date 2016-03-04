929c697f0cf72101c8f18f9522db629381f16412
Jonah Hontos <jonahhontos@gmail.com>
2016-03-04 15:21:43 -0800

    added comments + changed age to personality

--- models/Cat.js
+++ models/Cat.js
@@ -1,13 +1,16 @@
+///////// INITIALIZE REQS /////////
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
+///////// SPECIFY ATTRIBUTES /////////
 var catSchema = Schema({
-  _owner: {type: Schema.Types.ObjectId, ref: 'Owner'},
+  _owner: {type: Schema.Types.ObjectId, ref: 'Owner'}, // adds ref to Owner model
   name: {type: String, required: true},
   color: String,
-  age: Number
+  personality: String
 })
 
+///////// SEND CAT TO MODULE EXPORT /////////
 var Cat = mongoose.model('Cat', catSchema)
 
 module.exports = Cat
