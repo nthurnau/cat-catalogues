e819cfd7f715dd7b5c480465e1e93196502ff78c
Tune <Reidcrawford55@gmail.com>
2016-03-04 15:22:44 -0800

    model schema done for owners requesting a pull

--- models/Owner.js
+++ models/Owner.js
@@ -4,7 +4,7 @@ var express = require('express')
 var ownerSchema = new mongoose.Schema ({
   name: {type: String, required: true},
   address: String,
-  cats: [catSchema]
+  cats: [catSchema] // everything that goes in here is required by the cat schema
 })
 
 var Owner = mongoose.model('Album', ownerchema)
