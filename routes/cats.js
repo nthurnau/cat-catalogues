38fc8f4f9f5d9a95db625d603048c1f2e7ae9926
Andrew Wasicek <andrew.wasicek@gmail.com>
2016-03-04 15:23:09 -0800

    added routes for cats

--- routes/cats.js
+++ routes/cats.js
@@ -5,25 +5,51 @@ var request = require('request')
 
 //CAT ROUTES
 
-//index -- will be accessed through localhost:3000/cats/
-cat.get('/', function(err){
-  if(err) throw err
-  
+// Index -- shows all cats
+// Accessed through localhost:3000/cats
+cat.get('/', function(req, res){
+  Cat.find({}, function(err, cats){
+    if(err) throw err
+    res.json(cats)
+  })
 })
 
-//show -- will be accessed through localhost:3000/cats/:id
-cat.get('/:id')
+//Show -- shows a specific cat
+//Accessed through localhost:3000/cats/:id
+cat.get('/:id', function(req, res){
+  Cat.findOne({req.params.id}, function(err, cat){
+    if(err) throw err
+    res.json(cat)
+  })
+})
 
-//create
-cat.post
+//Create -- create a new cat
+//Accessed through localhost:3000/cats
+cat.post('/', function(req, res){
+  Cat.create(req.body, function(err, cat){
+    if(err) throw err
+    res.json({success: true, cat: cat})
+  })
+})
 
 
-//edit
-cat.patch
+//Edit -- edit an existing cat
+//Accessed through localhost:3000/cats/:id
+cat.patch('/:id', function(req, res){
+  Cat.findOneAndUpdate({_id: req.params.id}, req.body, {new:true}, function(err, cat){
+    if(err) throw err
+    res.json({success: true, cat: cat})
+  })
+})
 
+//Delete -- delete an existing cat
+//Accessed through localhost:3000/cats/:id
+cat.delete('/:id', function(req, res){
+  Cat.findOneAndRemove({_id: req.params.id}, function(err){
+    if(err) throw err
+    res.json({success: true, message: "Cat deleted."})
+  }
+})
 
-//delete
-cat.delete
-
-
+//Export so accessible to other files
 module.exports = cat
