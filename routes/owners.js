d3677786d0c20b1dbcbba93957589cd84458c29f
Jonah Hontos <jonahhontos@gmail.com>
2016-03-04 16:44:47 -0800

    added owner crud

--- routes/owners.js
+++ routes/owners.js
@@ -1,4 +1,40 @@
 var express = require('express')
-var owners = express.Router()
+var owner = express.Router()
+var Owner = require('../models/Owner.js')
 
-module.exports = owners
+owner.get('/', function(req,res){
+  Owner.find({}, function(err, owner){
+    if (err) throw err
+    res.json(owner)
+  })
+})
+
+owner.post('/', function(req,res){
+  Owner.create(req.body, function(err, owner){
+    if (err) throw err
+    res.json(owner)
+  })
+})
+
+owner.get('/:id', function(req,res){
+  Owner.findOne({_id: req.params.id}, function(err, owner){
+    if (err) throw err
+    res.json(owner)
+  })
+})
+
+owner.patch('/:id', function(req,res){
+  Owner.findOneAndUpdate({_id: req.params.id}, req.body, {new:true}, function(err, owner){
+    if (err) throw err
+    res.json(owner)
+  })
+})
+
+owner.delete('/:id', function(req,res){
+  Owner.findOneAndRemove({_id: req.params.id}, function(err){
+    if (err) throw err
+    res.json({message: "Owner annihilated!!"})
+  })
+})
+
+module.exports = owner
